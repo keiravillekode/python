7a7c001ec89f2bc1f0ebc3bff2b945c49e85baa6
Eric Willigers <keiravillekode@gmail.com>
2025-11-03 19:33:14 +1100

    Exhaustive breadth-first search

--- exercises/practice/hanoi/.meta/example.py
+++ exercises/practice/hanoi/.meta/example.py
@@ -1,13 +1,43 @@
-import functools
-
-@functools.lru_cache(maxsize=None)
 def moves(poles, discs):
-    if discs <= 1:
-        return discs
+    if discs == 0:
+        return 0
 
-    assert poles >= 3
+    # all on the first pole
+    start = tuple([2 ** discs - 1] + [0] * (poles - 1))
 
-    if poles == 3:
-        return 2 ** discs - 1
+    # all on the last pole
+    destination = tuple([0] * (poles - 1) + [2 ** discs - 1])
 
-    return min([2 * moves(poles, d) + moves(poles - 1, discs - d) for d in range(1, discs)])
+    count = 0
+    next_generation = [start]
+    seen = {start}
+
+    while True:
+        current_generation = next_generation
+        next_generation = []
+        count += 1
+        while len(current_generation) > 0:
+            current = current_generation.pop()
+            for source in range(poles):
+                # source pole empty?
+                if current[source] == 0:
+                    continue
+
+                disk = current[source] & (-current[source])
+                for sink in range(poles):
+                    # sink pole already contains a smaller disk?
+                    if sink == source or (current[sink] & (disk - 1)) != 0:
+                        continue
+
+                    values = list(current)
+                    values[source] -= disk
+                    values[sink] += disk
+                    state = tuple(values)
+                    if state in seen:
+                        continue
+
+                    if state == destination:
+                        return count
+
+                    seen.add(state)
+                    next_generation.append(state)
--- exercises/practice/hanoi/hanoi_test.py
+++ exercises/practice/hanoi/hanoi_test.py
@@ -10,16 +10,16 @@ class HanoiTest(unittest.TestCase):
         self.assertEqual(moves(2, 1), 1)
 
     def test_three_poles(self):
-        self.assertEqual(moves(3, 6), 63)
+        self.assertEqual(moves(3, 7), 127)
 
     def test_four_poles(self):
-        self.assertEqual(moves(4, 24), 513)
+        self.assertEqual(moves(4, 7), 25)
 
     def test_five_poles(self):
-        self.assertEqual(moves(5, 120), 7423)
+        self.assertEqual(moves(5, 7), 19)
 
     def test_six_poles(self):
-        self.assertEqual(moves(6, 720), 183297)
+        self.assertEqual(moves(6, 7), 17)
 
     def test_zero_discs(self):
         self.assertEqual(moves(9, 0), 0)
